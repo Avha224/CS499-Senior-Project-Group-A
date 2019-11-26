4937a9d439354e4e552f18689187121c51f970f1
Adrienne Corwin <adriennecorwin@gmail.com>
2019-11-26 11:00:08 -0500

    heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': os.environ['DATABASE_USER'],
         'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'PORT': '5432',
-        'OPTIONS': {'charset': 'utf8mb4'},
+        'HOST': 'ec2-54-225-195-3.compute-1.amazonaws.com'
         }
 }
 

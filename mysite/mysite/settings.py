703f4c8a081de87c5f24f81171e2c60964cc12d6
Adrienne Corwin <adriennecorwin@gmail.com>
2019-11-26 09:49:37 -0500

    heroku

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -125,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
